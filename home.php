52356320785cd2f49e0b6451fbd6040beb57e3e4
Joan Carballo <jcarbad@gmail.com>
2016-10-11 12:30:44 -0600

    Arreglos varios + Beautification
    Arreglos de path de recursos, de código y de formato

--- home.php
+++ home.php
@@ -1,58 +1,54 @@
 <?php
-//session_start();
 require_once('auth.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     <title>SISGENOT - Home </title>
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700" rel="stylesheet" type="text/css" />
-    <link href="./css/login.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-	 <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="./css/template/vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <link href="./css/template/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Bootstrap Core CSS -->
+    <link href="./css/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-
-<div id="wrapper">
-<table id = "ordenar">	
-<tr>	
-	<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="navbar-header">
-            
-                <a class="navbar-brand" href="http://localhost/SISGENOT/home.php">Sistema de Gesti&oacute;n de Notificaciones</a>
-                 <ul align="left">
-            <?php
-                echo "Bienvenido   ";
-                echo $_SESSION['SESS_ID'];
-            ?>
-        </ul></div>
-	
-		
-		<ul  align = "right">
-            <a  href="http://localhost/SISGENOT/home.php"><img src="./img/logo_login.png" width="100" height="100" /></a>
-        </ul>
-		</tr>
-              
-        </nav>
-		<table>
-               <td width = 200>       <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="http://localhost/SISGENOT/home.php">Crear Notificaci&oacute;n</a>
-                                </li></td>
-              <td width = 00>       <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="http://localhost/SISGENOT/home.php">Ver Notificaciones</a>
-                                </li></td>
-               </table>
-		
-		<div id="page-wrapper">
-            <div class="row">
-                
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
+    <div id="wrapper">
+        <table id = "ordenar">
+            <tr>
+                <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">Sistema de Gesti&oacute;n de Notificaciones</a>
+                        <ul align="left">
+                            <?php
+                            echo "Bienvenido   ", $_SESSION['SESS_ID'];
+                            ?>
+                        </ul>
+                    </div>
+                    <ul  align = "right">
+                        <a  href="#"><img src="./img/logo_login.png" width="100" height="100" /></a>
+                    </ul>
+                </nav>
+            </tr>
+            <tr>
+                <td width = "200">
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="#">Crear Notificaci&oacute;n</a>
+                        </li>
+                    </ul>
+                </td>
+                <td>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="#">Ver Notificaciones</a>
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                     <div class="panel panel-primary">
@@ -69,48 +65,42 @@ require_once('auth.php');
                         </div>
                         <a href="#">
                             <div class="panel-footer">
-                                <span class="pull-left">View las nuevas notificaciones</span>
+                                <span class="pull-left">Ver las nuevas notificaciones</span>
                                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                                 <div class="clearfix"></div>
                             </div>
                         </a>
                     </div>
                 </div>
-     <div class="row">
-                <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <div class="row">
-                                <div class="col-xs-3">
-                                    <i class="fa fa-comments fa-5x"></i>
-                                </div>
-                                <div class="col-xs-9 text-right">
-                                    <div class="huge">4</div>
-                                    <div>Expedientes</div>
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">4</div>
+                                        <div>Expedientes</div>
+                                    </div>
                                 </div>
                             </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Ver todos los expedientes</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">ver todos los expedientes</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
-                        </a>
                     </div>
                 </div>
-       </div>
-	       
-	</div>
-	   
-	   
-	   
-        <center>
-        
-            <h2><a href="index.php">LOG OUT</a></h2>
-        </center>
-    
-	</div>
-	
+            </div>
+            <center>
+                <h2><a href="index.php">LOG OUT</a></h2>
+            </center>
+        </div>
+    </div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <?php
-	//Start session
-	session_start();
-	//Unset the variables stored in session
-	unset($_SESSION['SESS_ID']);
-	unset($_SESSION['SESS_PASS']);
-    unset($_SESSION['SESS_TIPO']);
+//Start session
+session_start();
+//Unset the variables stored in session
+unset($_SESSION['SESS_ID']);
+unset($_SESSION['SESS_PASS']);
+unset($_SESSION['SESS_TIPO']);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Municipalidad de Mora</title>
-    
     <link href="./css/login.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 </head>
-
 <body>
     <div id="wrapper">
         <div align = "center" id="logo">
-		
             <img src="./img/logo_login.png" width="200" height="200" />
         </div>
         <h1 align="center">Ingreso al Sistema de Gesti&oacute;n de Notificaciones</h1>
@@ -41,25 +37,19 @@
                     </td>
                 </tr>
                 <tr>
-                        <td align="right">Usuario</td>
-                        <td> <input type="text" id="username" name="username" /></td>
-                    </tr>
-                    <tr>
-                        <td align="right">Contraseña </td>
-                        <td><input type="password" id="password" name="password" /></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" align="center"><input type="submit" value="Ingresar" class="btn" /></td>
-						
-					</tr>
- 
-
-		</a>
-	</div>
-                </table>
-            </form>
-            <center><a href="">¿Olvidó su contraseña?</a></center>
-        </div>
-    </body>
-
-    </html>
+                    <td align="right">Usuario</td>
+                    <td> <input type="text" id="username" name="username" /></td>
+                </tr>
+                <tr>
+                    <td align="right">Contraseña </td>
+                    <td><input type="password" id="password" name="password" /></td>
+                </tr>
+                <tr>
+                    <td colspan="2" align="center"><input type="submit" value="Ingresar" class="btn" /></td>
+                </tr>
+            </table>
+        </form>
+        <center><a href="">¿Olvidó su contraseña?</a></center>
+    </div>
+</body>
+</html>
