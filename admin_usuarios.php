9dcac72506326a10b90e620789da18632e23a54b
antonio39 <antonio39@live.com>
2016-11-03 18:54:41 -0600

    Ocultado de constraseña en tabla administrador
    no se muestra la constraña en la tabla administrador, en vez se muestra
    (*)

--- admin_usuarios.php
+++ admin_usuarios.php
@@ -301,7 +301,7 @@ $result = mysql_query($qry,$con);
                                   <td><?php echo $row[0]; ?></td>
                                   <td><?php echo $row[1]; ?></td>
                                   <td><?php echo $row[2]; ?></td>
-                                  <td><?php echo $row[3]; ?></td>
+                                  <td><?php echo str_replace($row[3],"********",$row[3]); ?></td>
                                   <td><?php echo ($row[4] == 1) ?"Administrador" : "Inspector" ; ?></td>
                                   <td>
                                     <button type="button" class="btn btn-info" onclick="location.href='http://localhost/MuniMora/editarUsuario.php?value=<?php echo $row[2];?>'">Editar</button>
