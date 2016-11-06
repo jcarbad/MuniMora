b1bac655edda47f8f4f51fd918b636c78f8041a6
antonio39 <antonio39@live.com>
2016-11-06 15:35:23 -0600

    Mensaje de eliminacion de usuarios, desde Administrador de sistema.
    Se muestra mensaje de eliminacion  de registro y redirecciona a la
    pagina vista de usuarios activos.

--- eliminarUsuario_exec.php
+++ eliminarUsuario_exec.php
@@ -10,13 +10,13 @@ $qry = "DELETE FROM usuarios WHERE ID = '$id' ";
 $sentencia = mysql_query($qry,$con);
 if($sentencia){	
 	echo '<script language="javascript">';
-	echo 'alert("Exitooo")';
+	echo 'if(alert("Exitooo")){location.href="admin_usuarios.php";}else{location.href="admin_usuarios.php"};';
 	echo '</script>';
-	header('Location: admin_usuarios.php');
+	//header('Location: admin_usuarios.php');
 
 } else{
 	echo '<script language="javascript">';
-	echo 'alert("ERROR")';
+	echo 'if(alert("ERROR")){location.href="admin_usuarios.php";}else{location.href="admin_usuarios.php";';
 	echo '</script>';
 }	
 ?>
